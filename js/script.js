a7c674cf7ea817ccc90ba3838c56e807ed91bdcb
Joel Medina <joelmedina2920@gmail.com>
2022-10-20 22:36:54 -0300

    tech-and-js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const icons = document.querySelector(".tech__icons ul").getElementsByTagName("li");
+
+/*if (icon === icons [0]){
+    icon.style.color = "red";
+    continue;
+}
+if (icon === icons [1]){
+    icon.style.color = "blue";
+    continue;
+}
+if (icon === icons [2]){
+    icon.style.color = "yellow";
+    continue;
+}
+if (icon === icons [3]){
+    icon.style.color = "pink";
+    continue;
+}
+if (icon === icons [4]){
+    icon.style.color = "red";
+    continue;
+}
+if (icon === icons [5]){
+    icon.style.color = "white";
+    continue;
+}*/
+
+/*for (const icon of icons){
+    if (icon === icons [0]){
+        
+        continue;
+    }else if (icon === icons [1]){
+        icon.lastChild.style.backgroundColor = "blue";
+        continue;
+    }else if (icon === icons [2]){
+        icon.style.color = "yellow";
+        continue;
+    }else if (icon === icons [3]){
+        icon.style.color = "pink";
+        continue;
+    }else if (icon === icons [4]){
+        icon.style.color = "red";
+        continue;
+    }else if (icon === icons [5]){
+        icon.style.color = "white";
+        continue;
+    }else {
+        break;
+    }
+    
+    console.log(icon)
+}
+*/
+
